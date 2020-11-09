a686faf08b5898dba14b3054ae28727137370220
rhiannonkimberlin <r.kim@live.com>
2020-11-09 09:55:04 -0700

    Adding API-Gateway Week 4

--- week-1/api-gateway/models/user.js
+++ week-1/api-gateway/models/user.js
@@ -5,4 +5,14 @@ var userSchema = new mongoose.Schema({
     email: String
 })
 
-module.exports = mongoose.model('User', userSchema);
+var User = module.exports = mongoose.model('user', userSchema);
+
+//user.save is used to add a new user in our database
+module.exports.add=(user, callback) => {
+    user.save(callback);
+};
+
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+}
